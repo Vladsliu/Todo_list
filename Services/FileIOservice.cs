01252a8500b623b840c38a9a8fd16857d9e74544
Vladsliu <vlad.sliusarch@gmail.com>
2023-01-03 11:12:59 +0100

    Todo_list
    Todo_list

---
+++ Services/FileIOservice.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo_sliu.Models;
+
+namespace Todo_sliu.Services
+{
+    class FileIOservice
+    {
+        private readonly string PATH;
+        public FileIOservice(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<Model_ToDo> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<Model_ToDo>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<Model_ToDo>>(fileText);
+            }
+        }
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
